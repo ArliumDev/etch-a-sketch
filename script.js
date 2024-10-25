40ce83585a5bc4b5e6de6044e059edc6c1fb9a08
ArliumDev <arliumdev@gmail.com>
2024-10-25 12:34:42 +0100

    pixel creation and random color painting working

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const clearButton = document.getElementById('clear');
 const resizeButton = document.getElementById('resize');
 const boardContainer = document.getElementById('board-container');
-let pixel; 
-const defaultValue = 16;
-const maxValue = 100;
+let pixelColumn;
+let pixelCount;
+let pixel;
+const defaultCount = 16;
+const maxCount = 100;
+
+
+
+// TODO: Change loop value to pixelCount
+
+/* Creates a 'n' numbers of columns to stock the pixels,
+   and then creates the pixels in every column */ 
 
 const createDefaultBoard = () => {
-  for (let i = 0; i < defaultValue; i++) {
-    pixel = document.createElement('div');
-    pixel.classList.add('pixel');
-    boardContainer.appendChild(pixel);
+  for (let i = 0; i < defaultCount; i++) {
+    pixelColumn = document.createElement('div');
+    pixelColumn.classList.add('pixel-column');
+    for (let j = 0; j < defaultCount; j++) {
+      pixel = document.createElement('div');
+      pixel.addEventListener('mouseover', randomColor);
+      pixel.classList.add('pixel');
+      pixelColumn.appendChild(pixel);
+    }
+    boardContainer.appendChild(pixelColumn); 
   }
+};
+
+/* Throws a random RGB color that will be used to paint */
+
+const randomColor = (e) => {
+  const r = Math.floor(Math.random() *  256);
+  const g = Math.floor(Math.random() *  256);
+  const b = Math.floor(Math.random() *  256);
+  // return `rgb(${r},${g},${b})`;
+  e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
 }
 
 createDefaultBoard();
+
+console.log(pixel);
